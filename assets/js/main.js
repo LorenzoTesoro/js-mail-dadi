18c28fd01fed9f4096c19b0ce7c1dfedf3906c94
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-11 13:47:57 +0200

    ex.1-mail: steps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-alert('hello');
+/*  Ex.1 : Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. */
+
+// 1. Creo un array in cui inserisco le mail di chi può accedere e salvo i valori in una variabile.
+
+// 2. Chiedo all'utente la mail - prompt e salvo l'inserimento in una variabile
+
+// 3. Se la mail è nella lista, allora stampo il mess di benvenuto - if statement
