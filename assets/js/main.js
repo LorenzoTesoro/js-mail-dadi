d73aa674885bd2314d6982d985de515dd647e38f
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-11 16:32:15 +0200

    ex2 with dom manipulation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /* Ex.1 : Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. */
 
-    const btn = document.querySelector("button");
+    /* const btn = document.querySelector("button");
 
     let title = document.querySelector("h1");
 
@@ -17,26 +17,27 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
         title.innerHTML = "Siamo spiacenti! Non hai l'invito";
     }
     })
+ */
 
 /* Ex.2 : Dadi 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+    const btn = document.querySelector("button");
 
-// 1. Creo un array che contenga i numeri d 1 a 6 e salvo il tutto in una variabile.
-    //const items = [1, 2, 3, 4, 5, 6];
-    //console.log(numbers);
-// 2. Genero numero random sia per giocatore 1 che per giocatore 2
-    //const randomNumbers = Math.floor(Math.random() * items.length + 1);
-    //console.log(randomNumbers);
-    //const player1 = Math.floor(Math.random()*items.length);
-    //const player2 = Math.floor(Math.random()*items.length);
-    //console.log(player1, player2);
-// 3. Confronto i valori ottenuti per stabilire quale sia più grande
-  /*   if(player1 > player2){
-        console.log("Vince player1");
-    } else if (player1 < player2){
-        console.log("Vince player2");
-    } else if (player1 == player2){
-        console.log('Pareggio');
-    }
- */
+    const player1 = document.querySelector(".player1 > span");
+    const player2 = document.querySelector(".player2 > span");
+
+    btn.addEventListener("click", function(){
+        const items = [1, 2, 3, 4, 5, 6];
+        player1.innerHTML = Math.floor(Math.random()* (items.length + 1));
+        player2.innerHTML = Math.floor(Math.random()* (items.length + 1));
+        let result = document.querySelector("p");
+
+        if (player1.innerHTML > player2.innerHTML){
+            result.innerHTML = "Vince player 1";
+        } else if (player1.innerHTML < player2.innerHTML){
+           result.innerHTML = "Vince player 2";
+        } else {
+            result.innerHTML = "Pareggio!";    
+        }
+    })
