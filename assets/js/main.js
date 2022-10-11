b0cc80490c444e5371d2dda662dbeab5dc788d79
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-11 13:58:37 +0200

    ex-mail: logging variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,17 @@
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. */
 
 // 1. Creo un array in cui inserisco le mail di chi può accedere e salvo i valori in una variabile.
+const mails = ["a@gmail.com", "b@gmail.com", "c@gmail.com", "d@gmail.com","e@gmail.com"];
+//console.log(mails);
 
 // 2. Chiedo all'utente la mail - prompt e salvo l'inserimento in una variabile
+const mail = prompt("Inserisci la tua mail");
+//console.log(mail);
 
 // 3. Se la mail è nella lista, allora stampo il mess di benvenuto - if statement
+
+if(mails.includes(mail)){
+    console.log("sei tra gli invitati");
+} else {
+    console.log("non sei tra gli invitati");
+}
